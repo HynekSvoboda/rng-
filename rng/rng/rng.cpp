b9fec051597ccb62171008677e548dda56e611ac
HynekSvoboda <97514770+HynekSvoboda@users.noreply.github.com>
2023-12-06 10:17:55 +0100

    Add files via upload

---
+++ rng/rng/rng.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+#include <ctime>  
+
+int main()
+{
+    srand(time(0));
+
+    cout << "Zadej dolni mez: ";
+    int a, b;
+    cin >> a;
+    cout << "Zadej horní mez: ";
+    cin >> b;
+    int pocet = b - a+1;
+    cout << a + rand() % +pocet << " ";
+    
+}
+
+
